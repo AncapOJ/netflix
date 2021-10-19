a6caecc95c4ca3ffd41e5afd3ee3d18b2161230a
EstherPE <esther.herraiz@bootcamp-upgrade.com>
2021-10-19 08:59:36 +0200

    zoom and try numbers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GlobalComponent } from './pages/global/global.component';
+import { NavComponent } from './pages/global/nav/nav.component';
+import { TopComponent } from './pages/global/top/top.component';
+import { GalleryComponent } from './pages/global/gallery/gallery.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GlobalComponent,
+    NavComponent,
+    TopComponent,
+    GalleryComponent,
   ],
   imports: [
     BrowserModule,
